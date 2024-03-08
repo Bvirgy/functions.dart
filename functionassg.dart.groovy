10a7f810b606f90e0d0d513a43f3929a832ab7a4
Bvirgy <162292592+Bvirgy@users.noreply.github.com>
2024-03-09 00:46:32 +0300

    Add files via upload

---
+++ functionassg.dart.groovy
@@ -0,0 +1,56 @@
+Task 1:
+To write the addTwo function in Dart, you can use the following code:
+
+dart
+int addTwo(int num1, int num2) {
+  return num1 + num2;
+}
+
+
+Task 2:
+For the subtractTwo function, you can use this code:
+
+dart
+int subtractTwo(int num1, int num2) {
+  return num1 - num2;
+}
+
+
+Task 3:
+To implement the multiplyTwo function, use this code:
+
+dart
+int multiplyTwo(int num1, int num2) {
+  return num1 * num2;
+}
+
+
+Task 4:
+For the divideTwo function, you can use this code:
+
+dart
+double divideTwo(double num1, double num2) {
+  return num1 / num2;
+}
+
+
+Task 5:
+To write the stringLength function, use this code:
+
+dart
+int stringLength(String text) {
+  return text.length;
+}
+
+
+Task 6:
+For the getFirstElement function, you can use this code:
+
+dart
+dynamic getFirstElement(List list) {
+  if (list.isEmpty) {
+    return null;
+  }
+  return list[0];
+}
+
